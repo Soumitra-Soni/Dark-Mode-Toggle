083890ad731dafd613471d6a3e673d433290501d
Soumitra-Soni <somitrasoni123@gmail.com>
2023-07-26 12:34:28 +0530

    done

---
+++ data.js
@@ -0,0 +1,30 @@
+const articles = [
+  {
+    id: 1,
+    title: "Introduction to Dark Mode Toggle",
+    date: new Date(2022, 9, 4),
+    length: 11,
+    snippet: `Explore the benefits and implementation of a dark mode toggle in web design. Learn how to enhance user experience and reduce eye strain with JavaScript-powered dark mode.`,
+  },
+  {
+    id: 2,
+    title: "Building a Custom Dark Mode Toggle",
+    date: new Date(2019, 10, 22),
+    length: 5,
+    snippet: `Step-by-step guide to create a personalized dark mode toggle using HTML, CSS, and JavaScript. Customize your website's appearance and delight users with a sleek dark mode feature.`,
+  },
+  {
+    id: 3,
+    title: "Optimizing Dark Mode for Accessibility",
+    date: new Date(2018, 7, 11),
+    length: 5,
+    snippet: `Discover accessibility best practices for dark mode implementation. Ensure your website is inclusive and user-friendly for all visitors, considering color contrast and readability.`,
+  },
+  {
+    id: 4,
+    title: "Dark Mode Toggle: Advanced Tips and Tricks",
+    date: new Date(2015, 5, 4),
+    length: 5,
+    snippet: ` Delve into advanced techniques to enhance your dark mode toggle. Implement local storage, smooth transitions, and UI improvements to create a seamless dark mode experience.`,
+  },
+];
